b532f0c5e84d3636d04429ac9b5b2821a582ed07
unknown <lequangphuoc2305@gmail.com>
2023-06-19 19:54:41 +0700

    upbaitap

--- module3/excercise_sql/student_management_query_function_19062023.sql
+++ module3/excercise_sql/student_management_query_function_19062023.sql
@@ -1,4 +1,4 @@
-use quanlysinhvien;
+use quan_ly_sinh_vien;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
 select *
@@ -12,8 +12,8 @@ join mark on subject.subid = mark.subid
 where mark=(select max(mark) from mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-select student.studentid, student.studentname, student.address, student.phone, student.status, student.classid, avg(mark.mark) as 'avg'
+select student.student_id, student.student_name, student.address, student.phone, student.status, student.classid, avg(mark.mark) as 'avg'
 from student
-join mark on student.studentid = mark.studentid
-group by student.studentid,student.studentname
+join mark on student.student_id = mark.student_id
+group by student.student_id,student.student_name
 
