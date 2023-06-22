4d1d836cb77eca91b4ac9f98d96a114e95449f8a
unknown <lequangphuoc2305@gmail.com>
2023-06-22 15:43:39 +0700

    upbaitap

--- module3/excercise_sql/100.case_study/100.case_study_query.sql
+++ module3/excercise_sql/100.case_study/100.case_study_query.sql
@@ -132,45 +132,160 @@ GROUP BY contract.contract_id;
 -- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
 --  (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
 
+create view other_quanlity_include_service as
+select detail_contract.service_id_include, sum(detail_contract.quanlity) as service_count
+from detail_contract 
+group by detail_contract.service_id_include
+having sum(detail_contract.quanlity) >= all (select sum(detail_contract.quanlity) from detail_contract 
+group by detail_contract.service_id_include);
+
+select include_service.service_id_include, include_service.service_name_include, other_quanlity_include_service.service_count
+from contract
+join detail_contract on contract.contract_id = detail_contract.contract_id
+join include_service on detail_contract.service_id_include = include_service.service_id_include
+join other_quanlity_include_service on include_service.service_id_include = other_quanlity_include_service.service_id_include
+group by include_service.service_id_include;
 
 -- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
 --  Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
 --  (được tính dựa trên việc count các ma_dich_vu_di_kem).
 
-select  contract.contract_id,service_type.service_type_name,  include_service.service_name_include
-from contract
-join detail_contract on contract.contract_id = detail_contract.contract_id
-join include_service on detail_contract.service_id_include = include_service.service_id_include
-join service on contract.service_id =service.service_id
-join rental_type on service.rental_type_id = rental_type.rental_type_id
+create view other_service_count as
+select detail_contract.service_id_include, COUNT(detail_contract.service_id_include) as ra_count
+from detail_contract 
+group by detail_contract.service_id_include;
+
+select contract.contract_id, service_type.service_type_name, include_service.service_name_include,other_service_count.ra_count
+from include_service
+join detail_contract on include_service.service_id_include = detail_contract.service_id_include
+join contract on detail_contract.contract_id = contract.contract_id
+join service on contract.service_id = service.service_id
 join service_type on service.service_type_id = service_type.service_type_id
-WHERE count(include_service.service_name_include) <2
+join other_service_count on include_service.service_id_include = other_service_count.service_id_include
+where other_service_count.ra_count =1
+order by contract_id asc;
 
 
 -- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do,
 --  ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+-- create view other_service_count as
+-- select detail_contract.service_id_include, COUNT(detail_contract.service_id_include) as ra_count
+-- from detail_contract 
+-- group by detail_contract.service_id_include;
 
+create view other_contract_count as 
+select contract.staff_id, COUNT(contract.staff_id) as count_of_contract
+from contract
+group by contract.staff_id;
+
+SELECT staff.staff_id, staff.full_name, specialize.specialize_name, department.department_name ,staff.phone_number,staff.locaion, contract.contract_id
+from staff
+join specialize on staff.specialize_id =specialize.specialize_id
+join department on staff.department_id =department.department_id
+join contract on staff.staff_id = contract.staff_id
+join other_contract_count on staff.staff_id =other_contract_count.staff_id
+where (year(contract.date_start_contract) = 2020 or year(contract.date_start_contract) = 2021) and other_contract_count.count_of_contract <=3
+order by staff_id asc;
 
 -- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+
+select *
+from staff
+left join contract on staff.staff_id = contract.staff_id;
+
+SET sql_safe_updates = 0;
+delete from staff
+where staff.staff_id not in (
+					select *
+                    from(
+					select staff.staff_id
+					from staff
+					join contract on staff.staff_id = contract.staff_id
+					where contract.date_start_contract between '2019-01-01' and '2021-12-31') as t);
+SET sql_safe_updates = 1;
+
+select *
+from staff
+left join contract on staff.staff_id = contract.staff_id;
 
 
 -- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond,
 --  chỉ cập nhật những khách hàng đã từng đặt phòng với Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
 
+create view update_customer as
+select customer.customer_id
+from customer	
+join customer_type on customer.customer_type_id = customer_type.customer_type_id
+join contract on customer.customer_id = contract.customer_id
+join detail_contract on contract.contract_id = detail_contract.contract_id
+join include_service on detail_contract.service_id_include = include_service.service_id_include
+join service on contract.service_id = service.service_id
+join rental_type on service.rental_type_id = rental_type.rental_type_id
+where year(contract.date_start_contract) = 2021 and customer_type.customer_type_name = 'platinium'
+group by customer.customer_id
+having sum(service.rental_costs + detail_contract.quanlity*include_service.price) >= 10000000;
+
+
+SET sql_safe_updates = 0;
+UPDATE customer
+join customer_type on customer.customer_type_id = customer_type.customer_type_id
+join contract on customer.customer_id = contract.customer_id
+join update_customer on customer.customer_id = update_customer.customer_id
+SET customer.customer_type_id = 1
+WHERE customer.customer_id in (
+	select *
+    from update_customer
+);
+SET sql_safe_updates = 1;							;
+
 
 -- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+
+CREATE view delete_customer as
+select customer.customer_id
+from customer 
+join contract on customer.customer_id = contract.customer_id
+where year(contract.date_start_contract) <2021;
+
+SET sql_safe_updates = 0;
+SET FOREIGN_KEY_CHECKS = 0;
+delete from customer
+where  customer.customer_id in (
+	SELECT *
+    from (
+	SELECT *
+	from delete_customer)  as p);
+SET FOREIGN_KEY_CHECKS = 1;
+SET sql_safe_updates = 1;
 
 
 -- 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
 
+SET sql_safe_updates = 0;
+UPDATE include_service
+join detail_contract on include_service.service_id_include = detail_contract.service_id_include
+join contract on detail_contract.contract_id = contract.contract_id
+set include_service.price = include_service.price*2
+where year(contract.date_start_contract) =2020 and detail_contract.quanlity >10;
+SET sql_safe_updates = 1;
+
 
 -- 20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, 
 -- thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
+select staff.staff_id, staff.full_name,staff.email,staff.phone_number,staff.date_of_birth,staff.locaion
+from staff
+union all
+select customer.customer_id,customer.full_name,customer.email,customer.phone_number,customer.date_of_birth,customer.location
+from customer;
 
 
 -- 21.	Tạo khung nhìn có tên là v_nhan_vien để lấy được thông tin của tất cả các nhân viên có địa chỉ là “Hải Châu”
 --  và đã từng lập hợp đồng cho một hoặc nhiều khách hàng bất kì với ngày lập hợp đồng là “12/12/2019”.
-
+CREATE view staff as
+SELECT *
+from staff
+join contract on staff.staff_id = contract.staff_id 
+where staff.locaion like '%hải châu%' and  DATE(contract.date_start_contract) = '2019-12-12'
 
 -- 22.	Thông qua khung nhìn v_nhan_vien thực hiện cập nhật địa chỉ thành “Liên Chiểu”
 --  đối với tất cả các nhân viên được nhìn thấy bởi khung nhìn này.
