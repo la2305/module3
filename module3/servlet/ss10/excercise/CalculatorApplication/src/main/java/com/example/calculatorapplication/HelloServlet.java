003c725f570545c2ec4dc43985cf3b47419a8618
unknown <lequangphuoc2305@gmail.com>
2023-07-01 12:57:00 +0700

    up

--- module3/servlet/ss10/excercise/CalculatorApplication/src/main/java/com/example/calculatorapplication/HelloServlet.java
+++ module3/servlet/ss10/excercise/CalculatorApplication/src/main/java/com/example/calculatorapplication/HelloServlet.java
@@ -1,26 +1,35 @@
 package com.example.calculatorapplication;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sql.rowset.serial.SerialException;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/calculate")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        float firstOperand = Integer.parseInt(request.getParameter("firstoperand"));
+        float secondOperand = Integer.parseInt(request.getParameter("secondoperand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>result:</h1>");
+        try {
+            float result = Calculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        } catch (Exception ex) {
+            writer.println("error: " + ex.getMessage());
+        }
+        writer.println("</html>");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
+
 
     public void destroy() {
     }
--- module3/servlet/ss10/excercise/CalculatorApplication/src/main/webapp/index.jsp
+++ module3/servlet/ss10/excercise/CalculatorApplication/src/main/webapp/index.jsp
@@ -2,12 +2,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>Calculator</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<h1>Simple Calculator</h1>
+<form method="post" action="/calculate">
+    <fieldset>
+        <legend>Calculator</legend>
+        <table>
+            <tr>
+                <td>First operand</td>
+                <td><input name="firstoperand" type="text"></td>
+            </tr>
+            <tr>
+                <td>Operator</td>
+                <td>
+                    <select name="operator">
+                        <option value="+">Addition</option>
+                        <option value="-">Subtraction</option>
+                        <option value="*">Multiplication</option>
+                        <option value="/">Division</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Second operand</td>
+                <td><input name="secondoperand" type="text"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input name="calculate" type="submit" value="calculate"></td>
+            </tr>
+        </table>
+    </fieldset>
+
+</form>
 </body>
 </html>
